4ad3575f06388cefc17b4182dee8e182ad417ee8
chathunga2007 <wggachathungabimsara2007@gmail.com>
2026-03-30 09:32:59 +0530

    intial commit

---
+++ JavaScript Basics/Week 11/String & Numbers/script.js
@@ -0,0 +1,35 @@
+// String Method
+
+let txt = " Hello World ";
+// 01. length()
+console.log(txt.length); //13
+// 04. trim()
+console.log(txt.trim()); //Hello World
+// 02. toUpperCase()
+console.log(txt.toUpperCase()); //HELLO WORLD
+// 03. toLowerCase
+console.log(txt.toLowerCase()); // hello world
+// 05. includes()
+console.log(txt.includes("Hello")); // true
+//06. slice()
+console.log(txt.slice(1, 8)); // Hello W
+
+// Template Literals
+let name = "Chathunga";
+let age = 18;
+
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+//Number Methods
+let num = 10.573;
+// 01. toFixed()
+console.log(num.toFixed(2)); //10.57
+// 02. parseInt()
+console.log(parseInt("10")); //10
+// 03. parseFloat()
+console.log(parseFloat("10.5")); //10.5
+// 04. Number()
+console.log(Number("10")); //10
+
+
+
