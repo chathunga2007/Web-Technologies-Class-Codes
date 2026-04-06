72c761db69b0d633555492eabd1abde3cc8bb72c
chathunga2007 <wggachathungabimsara2007@gmail.com>
2026-04-06 11:44:52 +0530

    initial commit

--- JavaScript Basics/Week 12/Task/script.js
+++ JavaScript Basics/Week 12/Task/script.js
@@ -1,6 +1,14 @@
+//01
 export const name = "John Doe";
 export const city = "New York";
 
+//02
 export function add(a, b) {
   return a + b;
+}
+
+//03
+export const number = 42;
+export default function multiply(a, b) {
+  return a * b;
 }
--- JavaScript Basics/Week 12/Task/script2.js
+++ JavaScript Basics/Week 12/Task/script2.js
@@ -1,11 +1,17 @@
 //Task 01
-import { name, city } from "./script";
+import { name, city } from "./script.js";
 
 console.log(name); // Output: John Doe
 console.log(city); // Output: New York
 
 //Task 02
 
-import { add } from "./script";
+import { add } from "./script.js";
 
 console.log(add(2, 3)); // Output: 5
+
+//Task 03
+import multiply, { number } from "./script.js";
+
+console.log(number); // Output: 42
+console.log(multiply(2, 3)); // Output: 6
